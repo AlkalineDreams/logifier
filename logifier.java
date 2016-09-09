61a3db1ebc8395c424d9c47a5ce01f9ca6fd861c
AlkalineDreams <craig.dwyer@ocsbstudent.ca>
2016-09-08 20:05:07 -0400

    Create logifier.java

---
+++ logifier.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class TheLogifier {
+
+
+ public static void main(String[] args) throws IOException {
+ double length;
+ double logs;
+  InputStreamReader r = new InputStreamReader(System.in);
+  BufferedReader br = new BufferedReader(r);
+
+  System.out.println("Are your logs 0.25m, 0.5m or 1m long?");
+
+  length =  Double.parseDouble(br.readLine());
+
+  logs = 1100.0/(20.0*length);
+
+  System.out.println(logs);
+
+ }
+
+}
